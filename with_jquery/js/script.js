f69f2290bc288f391490b9654783668a28bf34d8
Cclass14\Administrator <xyk32@naver.com>
2017-09-30 11:57:23 +0900

    no message

---
+++ with_jquery/js/script.js
@@ -0,0 +1,31 @@
+//var doc = document;
+//
+//doc.getElementById('add').addEventListener('click',function(){
+//    
+//    var value = doc.getElementById('stack').value;
+//    var list = doc.getElementById('stackList');
+//    
+//    var item = doc.createElement('div');
+//    item.innerHTML = value;
+//    
+//    list.appendChild(item);
+//    
+//});
+
+//$('#add').on('click',function(){
+//    
+//    var value = $('#stack').val();
+//    var list = $('#stackList');
+//    
+//    var item = $('<div>');
+//    item.text(value);
+//    
+//    list.append(item);
+//});
+
+var item = [ 'HTML' , 'CSS' ];
+
+var item1 = $('<div>').text(item[0]);
+var item2 = $('<div>').text(item[1]);
+
+$('#stackList').append();
