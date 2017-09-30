4bba6ad3e569eebcd9b8b2d8fd418c2e3dfe9bd5
Cclass14\Administrator <xyk32@naver.com>
2017-09-30 12:14:13 +0900

    no message

--- with_jquery/js/script.js
+++ with_jquery/js/script.js
@@ -1,45 +1,52 @@
-//var doc = document;
-//
-//doc.getElementById('add').addEventListener('click',function(){
-//    
-//    var value = doc.getElementById('stack').value;
-//    var list = doc.getElementById('stackList');
-//    
-//    var item = doc.createElement('div');
-//    item.innerHTML = value;
-//    
-//    list.appendChild(item);
-//    
-//});
+/* var doc = document;
 
-//$('#add').on('click',function(){
-//    
-//    var value = $('#stack').val();
-//    var list = $('#stackList');
-//    
-//    var item = $('<div>');
-//    item.text(value);
-//    
-//    list.append(item);
-//});
+doc.getElementById('add').addEventListener('click',function(){
+    
+    var value = doc.getElementById('stack').value;
+    var list = doc.getElementById('stackList');
+    
+    var item = doc.createElement('div');
+    item.innerHTML = value;
+    
+    list.appendChild(item);
+    
+});
+
+$('#add').on('click',function(){
+    
+    var value = $('#stack').val();
+    var list = $('#stackList');
+    
+    var item = $('<div>');
+    item.text(value);
+    
+    list.append(item);
+});
+*/
 
 var item = [ 'HTML' , 'CSS' ]; //백엔드로 부터 받은 데이터
 
 function view(arr){
+    
+    $('#stackList').empty();
  
     for ( var i = 0; i < arr.length; i++ ) {
         
         var tag = $('<div>').text(arr[i]);
-        $('#stackList').empty().append(tag);
+        $('#stackList').append(tag);
     }
 }
 
 $('#add').on('click',function(){
     
+    var value = $('#stack').val();
+    item.push(value);
+    
     view(item);
 });
 
 view(item);
+
 
 /* var item1 = $('<div>').text(item[0]);
 var item2 = $('<div>').text(item[1]);
